5a3207c093419bab63bae87a174e1c585dc9f85e
Tushar H <tusharjain2201@gmail.com>
2024-08-27 04:55:25 +0530

    Add changes to make component portable

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  // <StrictMode>
-    <App />
-  // </StrictMode>,
-)
+let widgetDivs = document.querySelectorAll('.performance-widget');
+
+widgetDivs.forEach((div) => {
+  createRoot(div!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
